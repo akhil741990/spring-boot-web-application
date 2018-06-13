40928adf9b7095af08816723a1dc641cf7b7d1ff
Akhil Pillai <akhil741990@gmail.com>
2018-06-13 10:53:35 +0530

    Added modifiable list for elements

--- src/com/soul/springboot/app/element/ElementService.java
+++ src/com/soul/springboot/app/element/ElementService.java
@@ -1,5 +1,6 @@
 package com.soul.springboot.app.element;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ElementService {
 
-	private List<Element> elements = Arrays.asList(new Element("Earth",0,"Prithvi"),
+	private List<Element> elements =    new ArrayList<>(
+			Arrays.asList(new Element("Earth",0,"Prithvi"),
 			new Element("Water", 1, "Jal"),
 			new Element("Air",2,"Vayu"),
 			new Element("Fire", 3, "Agni"),
-			new Element("Ether",4,"Aakash"));
+			new Element("Ether",4,"Aakash")));
 	
 	public List<Element> getElements(){
 		return elements;
